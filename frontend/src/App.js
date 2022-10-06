e7a3c926ba8dac647d299371d5ad78fe117b7d29
Amar <augustine2903a@gmail.com>
2022-10-06 11:56:30 -0400

    Create Components folder and set Header,Footer components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,19 @@
-
-import './App.css';
+import { Container } from 'react-bootstrap';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="App">
-    <h1>Welcome to my App</h1>
-    </div>
+    <>
+      <Header />
+      <main>
+        <Container>
+          <h1>Welcome to my App </h1>
+        </Container>
+      </main>
+     
+      <Footer />
+    </>
   );
 }
 
